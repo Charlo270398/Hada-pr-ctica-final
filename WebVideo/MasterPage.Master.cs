68405c1fa35017b7ca3d15a7bf2fa1c61ad6a485
Carlos <Carlos@PC-GARAJE>
2018-05-10 22:19:10 +0200

    inicio

---
+++ WebVideo/MasterPage.Master.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Clases.EN;
+using CAD;
+using System.Drawing;
+
+namespace WebVideo
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Charset = "utf-8";
+            usuarioEN user = (usuarioEN)Session["user_session_data"];
+            if (user != null)
+            {
+                menuMantenimiento.Visible = true;
+                menuSalir.Visible = true;
+                menuLogin.Visible = false;
+                menuRegistro.Text = "Área cliente";
+                menuRegistro.NavigateUrl = "Area_cliente.aspx";
+                Nombre.Visible = true;
+                Nombre.ForeColor = Color.Black;
+                Nombre.Text = "Sesión iniciada como: " + user.Email;
+            }
+            else
+            {
+                Nombre.Visible = false;
+                menuRegistro.NavigateUrl = "Registro.aspx";
+                menuRegistro.Text = "Registro";
+                menuMantenimiento.Visible = false;
+                menuSalir.Visible = false;
+                menuLogin.Visible = true;
+            }
+        }
+
+        protected void accederButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void nuevoUsuarioButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void menuMantenimiento_Init(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void a(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
