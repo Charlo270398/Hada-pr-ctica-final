27cb02c9c9bab45afd204009a3dcfa3b8ee827cf
Carlos <Carlos@PC-GARAJE>
2018-05-15 18:47:09 +0200

    Operativo

---
+++ Clases/CAD/ItransaccionCAD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases.EN;
+
+namespace CAD
+{
+    public interface ItransaccionSerieCAD
+    {
+        void alquilar();
+        void devolver();
+        void comprar();
+        void modificarCompra();
+        void modificarAlquiler();
+        transaccionSerieEN mostrarTransaccion();
+    }
+}
