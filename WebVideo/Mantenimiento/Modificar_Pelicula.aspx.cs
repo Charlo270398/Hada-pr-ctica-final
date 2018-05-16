390daa81b0fec01c9aef0211f2b76b417406a7b4
Carlos <Carlos@PC-GARAJE>
2018-05-17 01:31:39 +0200

    modificar pelicula

--- WebVideo/Mantenimiento/Modificar_Pelicula.aspx.cs
+++ WebVideo/Mantenimiento/Modificar_Pelicula.aspx.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Clases.EN;
 
 namespace WebVideo.Mantenimiento
 {
     public partial class Modificar_Pelicula : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void DWdia_Init(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DWCargar_Init(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_Modificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_Cargar_Click(object sender, EventArgs e)
         {
 
         }
