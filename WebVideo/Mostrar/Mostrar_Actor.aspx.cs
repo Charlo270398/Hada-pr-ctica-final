be2979b49e7faf600b5ece6eb16879def1f61ce2
Carlos <Carlos@PC-GARAJE>
2018-05-25 10:47:22 +0200

    Arreglos CAD/EN en mostrar

--- WebVideo/Mostrar/Mostrar_Actor.aspx.cs
+++ WebVideo/Mostrar/Mostrar_Actor.aspx.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using CAD;
 using Clases.EN;
 
 namespace WebVideo.Mostrar
 {
     public partial class Mostrar_Actor : System.Web.UI.Page
     {
-        actorEN actor = new actorEN();
-        List<int> listaID = new List<int>();
+        actorEN actor = new actorEN();//Actor a mostrar
+        List<int> listaID = new List<int>();//Lista con id asociados para peliculas
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -20,14 +19,21 @@ namespace WebVideo.Mostrar
 
         protected void Btn_PeliculaC(object sender, EventArgs e)
         {
-            if (DWPeliculas.SelectedItem.ToString() != "[Seleccionar]")
+            try
             {
-                Response.Redirect("Mostrar_Peliculas.aspx?id=" + listaID[DWPeliculas.SelectedIndex - 1]);
+                if (DWPeliculas.SelectedItem.ToString() != "[Seleccionar]")
+                {
+                    Response.Redirect("Mostrar_Peliculas.aspx?id=" + listaID[DWPeliculas.SelectedIndex - 1]);
+                }
+                else
+                {
+                    ErrPelicula.Visible = true;
+                    ErrPelicula.Text = "*Seleccione una película";
+                }
             }
-            else
+            catch (Exception ex)
             {
-                ErrPelicula.Visible = true;
-                ErrPelicula.Text = "*Seleccione una película";
+                Response.Redirect("../Pagina_Error.aspx?err=" + ex.Message);
             }
         }
 
@@ -36,23 +42,21 @@ namespace WebVideo.Mostrar
             try
             {
                 int id;
-                int.TryParse(Request.QueryString["id"], out id);
-                actor.IdAc = id;
-                actorCAD aux = new actorCAD();
-                peliculaCAD paux = new peliculaCAD();
-                actor = aux.mostrarActor(actor.IdAc);
+                int.TryParse(Request.QueryString["id"], out id);//Recuperamos id del actor
+                actor = new actorEN(id,"");//Cargamos id del actor en el EN
+                actor = actor.mostrarActor();//Cargamos los datos del actor
                 NombreText.Text = actor.Nombre;
                 ApellidosText.Text = actor.Apellidos;
                 fechaNac.Text = actor.FechaNac.Substring(0, 10);
-                paisCAD p = new paisCAD();
-                nombrePais.Text = p.mostrarIdPais(actor.Pais).Pais;
+                paisEN p = new paisEN(actor.Pais);
+                nombrePais.Text = p.mostrarIdPais().Pais;
 
-                List<peliculaEN> peliculas = aux.peliculasActor(actor.IdAc);
+                List<peliculaEN> peliculas = actor.mostrarPeliculasActor();
                 List<string> nombres = new List<string>();
-                for (int i = 0; i < peliculas.Count; i++)
+                for (int i = 0; i < peliculas.Count; i++)//Guardamos nombres en la lista desplegable
                 {
                     nombres.Add(peliculas[i].NombreP);
-                    listaID.Add(peliculas[i].IdP);
+                    listaID.Add(peliculas[i].IdP);//Asociamos id a cada nombre de la lista
                 }
                 DWPeliculas.DataSource = nombres;
                 DWPeliculas.DataBind();
--- WebVideo/Mostrar/Mostrar_Peliculas.aspx.cs
+++ WebVideo/Mostrar/Mostrar_Peliculas.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using CAD;
 using Clases.EN;
 
 namespace WebVideo.Peliculas
@@ -27,37 +26,37 @@ namespace WebVideo.Peliculas
         {
             try
             {
-                directorCAD d = new directorCAD();
-                DistribuidoraCAD dis = new DistribuidoraCAD();
+                
                 int id;
-                int.TryParse(Request.QueryString["id"], out id);
-                pelicula = new peliculaEN(id, "");
-                pelicula = pelicula.mostrarPelicula();
+                int.TryParse(Request.QueryString["id"], out id);//Recuperamos Id de la pelicula
+                pelicula = new peliculaEN(id, "");//Cargamos id de la pelicula
+                pelicula = pelicula.mostrarPelicula();//Cargamos datos de la pelicula
+                directorEN d = new directorEN(pelicula.IdDir);
+                distribuidoraEN dis = new distribuidoraEN(pelicula.IdDist,"");
                 Titulo.Text = pelicula.NombreP;
                 Texto_Sinopsis.Text = pelicula.Sinopsis;
                 Imagen.ImageUrl = pelicula.Imagen;
-                float precio = pelicula.PrecioA / 100;
+                float precio = pelicula.PrecioA / 100;//Conversion a euros
                 precioAnumtext.Text = precio.ToString() + "€";
                 fechaEstrenotext.Text = pelicula.FechaE.Substring(0, 10);
                 precio = pelicula.PrecioC / 100;
                 precioCnumtext.Text = precio.ToString() + "€";
                 TrailerLink.NavigateUrl = pelicula.Trailer;
                 duracionNumtext.Text = pelicula.Duracion.ToString() + " min";
-                directorEN director = new directorEN(pelicula.IdDir);
-                mostrarDirText.Text = d.mostrarDirector(director).Nombre + " " + d.mostrarDirector(director).Apellidos;
+                mostrarDirText.Text = d.mostrarDirector().Nombre + " " + d.mostrarDirector().Apellidos;
                 mostrarDirText.NavigateUrl = "Mostrar_Director.aspx?id=" + pelicula.IdDir;
-                mostrarDistText.Text = dis.mostrarDistribuidora(pelicula.IdDist).Nombre;
+                mostrarDistText.Text = dis.mostrarDistribuidora().Nombre;
                 mostrarDistText.NavigateUrl = "Mostrar_Distribuidora.aspx?id=" + pelicula.IdDist;
                 if (pelicula.IdSaga != -1)
                 {
-                    sagaCAD s = new sagaCAD();
+                    sagaEN s = new sagaEN(pelicula.IdSaga, "","");
                     mostrarSaga.Visible = true;
                     SagaText.Visible = true;
-                    mostrarSaga.Text = s.mostrarSaga(pelicula.IdSaga).Nombre;
+                    mostrarSaga.Text = s.mostrarSaga().Nombre;
 
                 }
                 Response.Charset = "utf-8";
-                usuarioEN user = (usuarioEN)Session["user_session_data"];
+                usuarioEN user = (usuarioEN)Session["user_session_data"];//Cargamos el usuario si hay sesion iniciada
                 if (user != null)
                 {
                     adquirirText.Visible = true;
